6406ba52c6162f0e70ed946ec028b4551b408b35
Kaustubh Hassan Narasimhan <kaustubh.hassannarasimhan@sjsu.edu>
2019-01-13 19:02:18 -0800

    Adding isIdentical and isSubtree Methods

--- BinarySearchTree/BinarySearchTree.java
+++ BinarySearchTree/BinarySearchTree.java
@@ -337,4 +337,26 @@ public class BinarySearchTree {
 
         return n;
     }
+
+    public boolean isIdentical(Node root1, Node root2){
+        if(root1 == null && root2 == null)
+            return true;
+        else if(root1 == null && root2 != null)
+            return false;
+        else if(root1 != null && root2 == null)
+            return false;
+        if(root1.data == root2.data)
+            return isIdentical(root1.left, root2.left) && isIdentical(root1.right, root2.right);
+        return false;
+    }
+
+    public boolean isSubtree(Node root1, Node root2){
+        if(root2 == null)
+            return true;
+        if(root1 == null)
+            return false;
+        if(isIdentical(root1, root2))
+            return true;
+        return isSubtree(root1.left, root2) || isSubtree(root1.right, root2);
+    }
 }
